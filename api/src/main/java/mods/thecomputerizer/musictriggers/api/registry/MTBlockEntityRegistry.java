5d5683be66e9e862acda2899ac05bd14f40391a0
TheComputerizer <dcpixelation42@gmail.com>
2025-01-29 11:58:38 -0600

    Update Gradle to 8.12.1 & fix wiki plugin

--- api/src/main/java/mods/thecomputerizer/musictriggers/api/registry/MTBlockEntityRegistry.java
+++ api/src/main/java/mods/thecomputerizer/musictriggers/api/registry/MTBlockEntityRegistry.java
@@ -21,6 +21,7 @@ import java.util.Objects;
 
 import static mods.thecomputerizer.musictriggers.api.registry.MTBlockRegistry.MUSIC_RECORDER;
 import static mods.thecomputerizer.musictriggers.api.registry.MTItemRegistry.ENHANCED_MUSIC_DISC;
+import static mods.thecomputerizer.theimpossiblelibrary.api.world.BlockPosAPI.ZERO;
 
 public class MTBlockEntityRegistry {
     
@@ -49,8 +50,8 @@ public class MTBlockEntityRegistry {
                 }
             }).build();
     
-    public static BlockEntityAPI<?,?> createRecorderEntity(WorldAPI<?> ignored1, BlockStateAPI<?> ignored2) {
-        return MUSIC_RECORDER_ENTITY.createFromReference();
+    public static BlockEntityAPI<?,?> createRecorderEntity(WorldAPI<?> world, BlockStateAPI<?> state) {
+        return MUSIC_RECORDER_ENTITY.createFromReference(world,ZERO,state);
     }
     
     public static @Nullable PlayerAPI<?,?> findClosestPlayerInWorld(
